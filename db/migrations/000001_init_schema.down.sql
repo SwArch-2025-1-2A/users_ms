ace05d7fd749bb87b27198fb10d2caad4702203e
arojasag <arojasag@unal.edu.co>
2025-06-13 23:35:47 -0500

    Update database schema

--- db/migrations/000001_init_schema.down.sql
+++ db/migrations/000001_init_schema.down.sql
@@ -1,3 +1,6 @@
+-- Used to automatically generate UUIDs for new Categories
+DROP EXTENSION IF EXISTS "uuid-ossp";
+
 -- These two tables are the most directly related to a user's profile
 -- I assume that other tables that have some relationship to users (joinRequests, member,
 -- participant) belong to the groups microservice
