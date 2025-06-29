72b8caafd0c1c3f0fa1baed2e2e82afbf88f5db6
arojasag <arojasag@unal.edu.co>
2025-06-29 15:43:58 -0500

    Change the createUser endpoint to reflect the fact that the profile picture is not uploaded during signup

--- handlers/users.go
+++ handlers/users.go
@@ -2,10 +2,8 @@ package handlers
 
 import (
 	"database/sql"
-	"io"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/SwArch-2025-1-2A/users_ms/repository"
 	"github.com/gin-gonic/gin"
@@ -13,9 +11,8 @@ import (
 )
 
 type UserResponse struct {
-	ID            uuid.UUID `json:"id"`
-	Name          string    `json:"name"`
-	ProfilePicUrl string    `json:"profilePicUrl"`
+	ID   uuid.UUID `json:"id"`
+	Name string    `json:"username"`
 }
 
 func CreateUserHandler(c *gin.Context) {
@@ -25,48 +22,18 @@ func CreateUserHandler(c *gin.Context) {
 		return
 	}
 
-	idStr := c.PostForm("id")
-	if idStr == "" {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "You didn't enter an id (uuid)"})
-		return
-	}
-	id, err := uuid.Parse(idStr)
+	var requestBody UserResponse
+	err := c.BindJSON(&requestBody)
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "The given id is not a valid UUID"})
-		return
-	}
-	name := c.PostForm("name")
-
-	file, header, err := c.Request.FormFile("profilePic")
-	var profilePic []byte
-	// A missing profilePic shouldn't cause any errors. It should be possible
-	// to create a user without a profilePic and leave that for later
-	if err == http.ErrMissingFile {
-		profilePic = nil
-	} else if err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Strange error while reading the profile pic"})
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Expected two parameters in body: id (a valid UUID) and username"})
 		log.Println(err.Error())
 		return
-	} else {
-		defer file.Close()
-		if !strings.HasPrefix(header.Header.Get("Content-Type"), "image/") {
-			c.IndentedJSON(http.StatusBadRequest,
-				gin.H{"error": "The attached file isn't an image. Make sure that the content-type header is set to image/"})
-			return
-		}
-
-		profilePic, err = io.ReadAll(file)
-		if err != nil {
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Error when trying to read the image"})
-			log.Println(err.Error())
-			return
-		}
 	}
 
 	args := repository.CreateUserParams{
-		ID:         id,
-		Name:       name,
-		ProfilePic: profilePic,
+		ID:         requestBody.ID,
+		Name:       requestBody.Name,
+		ProfilePic: nil,
 	}
 
 	user, err := app.Queries.CreateUser(app.Context, args)
@@ -76,13 +43,18 @@ func CreateUserHandler(c *gin.Context) {
 		return
 	}
 
-	userResponse := UserResponse{
-		ID:            user.ID,
-		Name:          user.Name,
-		ProfilePicUrl: GenerateImageURL(user.ID),
+	response := UserResponse{
+		ID:   user.ID,
+		Name: user.Name,
 	}
 
-	c.IndentedJSON(http.StatusCreated, gin.H{"status": "success", "data": userResponse})
+	c.IndentedJSON(http.StatusCreated, gin.H{"status": "success", "data": response})
+}
+
+type GetUserResponse struct {
+	ID            uuid.UUID `json:"id"`
+	Name          string    `json:"username"`
+	ProfilePicUrl string    `json:"profilePicUrl"`
 }
 
 func GetUserHandler(c *gin.Context) {
@@ -107,7 +79,7 @@ func GetUserHandler(c *gin.Context) {
 		return
 	}
 
-	response := UserResponse{
+	response := GetUserResponse{
 		ID:            user.ID,
 		Name:          user.Name,
 		ProfilePicUrl: GenerateImageURL(user.ID),
@@ -152,7 +124,7 @@ func ChangeUserName(c *gin.Context) {
 		return
 	}
 
-	response := UserResponse{
+	response := GetUserResponse{
 		ID:            user.ID,
 		Name:          user.Name,
 		ProfilePicUrl: GenerateImageURL(user.ID),
